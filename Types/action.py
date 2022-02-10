d44309d7d6c50a65f02d746c5fdcf7b3f61bff4d
Eric Slutz <eric.slutz@icloud.com>
2022-02-10 08:52:25 -0500

    updated code ql config and moved enums into main file

--- Types/action.py
+++
@@ -1,8 +0,0 @@
-"""Enum for creating an Action type."""
-from enum import Enum
-
-
-class Action(Enum):
-    """Define values for the Action type."""
-    GET = 'get'
-    GO = 'go'
--- text_based_game.py
+++ text_based_game.py
@@ -1,1 +1,29 @@
-# Eric Slutz
+"""Space Adventure Text Based Game"""
+from enum import Enum
+
+
+class Action(Enum):
+    """Define values for the Action type."""
+    GET = 'get'
+    GO = 'go'
+
+
+class Direction(Enum):
+    """Define values for the Direction type."""
+    EAST = 'East'
+    NORTH = 'North'
+    SOUTH = 'South'
+    WEST = 'West'
+
+
+class Item(Enum):
+    """Define values for the Item type."""
+    ACCESS_CARD = 'Access Card'
+    FIRST_AID_KIT = 'First Aid Kit'
+    FLUX_CAPACITOR = 'Flux Capacitor'
+    POWERED_ARMOR = 'Powered Armor'
+    SOCKS = 'Socks'
+    SONIC_SCREWDRIVER = 'Sonic Screwdriver'
+    SPACE_GLUE = 'Space Glue'
+    SPACE_SNACKS = 'Space Snacks'
+    SPARE_PARTS = 'Spare Parts'
